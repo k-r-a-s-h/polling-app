91bb79f36957c12c943a9a973df639c8f7239f61
Krishna Gupta <krishna05564@gmail.com>
2024-01-21 00:23:38 +0530

    🗃️ Add Poll and answers table

---
+++ prisma/migrations/20240120185137_create_poll_and_answers/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE `Poll` (
+    `id` VARCHAR(191) NOT NULL,
+    `question` LONGTEXT NOT NULL,
+    `expiry_date` DATETIME(3) NOT NULL,
+    `createdBy` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Answers` (
+    `id` VARCHAR(191) NOT NULL,
+    `poll_id` VARCHAR(191) NOT NULL,
+    `answer` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Poll` ADD CONSTRAINT `Poll_createdBy_fkey` FOREIGN KEY (`createdBy`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Answers` ADD CONSTRAINT `Answers_poll_id_fkey` FOREIGN KEY (`poll_id`) REFERENCES `Poll`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
