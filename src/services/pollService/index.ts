f2627792171b080b0070039247b6497e29af9a75
Krishna Gupta <krishna05564@gmail.com>
2024-01-21 01:37:35 +0530

    👔 Add Poll Service

---
+++ src/services/pollService/index.ts
@@ -0,0 +1,19 @@
+import { PollRepository } from "../../models/Poll";
+import { Poll, PollDTO } from "./types";
+
+export class PollService {
+    private pollRepository: PollRepository
+    constructor() {
+        this.pollRepository = new PollRepository();
+    }
+    createPollAndAnswers = async (pollRequest: PollDTO): Promise<Poll> => {
+        try {
+            //TODO: validate poll request
+            const poll = await this.pollRepository.createPoll(pollRequest);
+            return poll;
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    }
+}
