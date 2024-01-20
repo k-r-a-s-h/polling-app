cfeb84f50d89e30b30a26f4f02a75d68b3a81f79
Krishna Gupta <krishna05564@gmail.com>
2024-01-20 23:47:04 +0530

    🗃️  Add user repository

---
+++ src/models/User/index.ts
@@ -0,0 +1,30 @@
+import prisma from "../prisma";
+import { User, UserDTO } from "../../services/userService/types";
+
+export class UserRepository {
+    async createUser(userRequest:UserDTO):Promise<User> {
+        try {
+        const user = await prisma.user.create({
+            data: userRequest
+        });
+        return user;
+        } catch (err){
+            console.error(`[UserRepository.createUser]`,err);
+            throw err;
+        }
+    }
+
+    async getUserById(id:string):Promise<User | null>{
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    id
+                }
+            });
+            return user;
+        } catch (err){
+            console.error(`[UserRepository.getUserById]`,err);
+            throw err;
+        }
+    }
+}
