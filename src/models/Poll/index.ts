c6f387a07b8aaaf2c2fd08a372e69b217e225683
Krishna Gupta <krishna05564@gmail.com>
2024-01-21 01:35:49 +0530

    🗃️ Add Poll Repository

---
+++ src/models/Poll/index.ts
@@ -0,0 +1,28 @@
+import { Poll, PollDTO } from "../../services/pollService/types";
+import prisma from "../prisma";
+
+export class PollRepository {
+    async createPoll(pollRequest:PollDTO):Promise<Poll> {
+        try {
+            const poll = await prisma.poll.create({
+                data: {
+                    description: pollRequest.description,
+                    expiryDate: pollRequest.expiryDate,
+                    question: pollRequest.question,
+                    // Hardcoding value till we implement user authentication 
+                    createdBy: "5ac0dff3-8085-49d7-b69d-a2d070702265",
+                    answers: {
+                        create: pollRequest.answers
+                    }
+                },
+                include: {
+                    answers: true
+                }
+            },);
+            return poll;
+        } catch (err) {
+            console.error(`[PollRepository.createPoll]`,err);
+            throw err;
+        }
+    }
+}
