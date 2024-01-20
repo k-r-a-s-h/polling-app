d3fbd175c3e0704fc81d045ca985550321ce3941
Krishna Gupta <krishna05564@gmail.com>
2024-01-20 23:51:07 +0530

    🧱 Add User Routes

---
+++ src/routes/user.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import UserController from "../controller/UserController";
+
+const router= Router();
+const userController = new UserController();
+router.post("/",userController.createUser);
+export default  router;
