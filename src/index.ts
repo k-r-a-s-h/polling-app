68be70f37657e1f7b74d0457dc6a261239685607
Krishna Gupta <krishna05564@gmail.com>
2024-01-21 01:40:44 +0530

    🧱 Add Poll Router

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { json } from "body-parser";
 import express from "express";
 import { default as userRouter}  from "./routes/user";
-
+import {default as pollRouter} from "./routes/poll"
 // setup express app
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +9,6 @@ app.use(json());
 
 // setup routes
 app.use('/user', userRouter);
-
+app.use('/poll', pollRouter);
 // setup server port
 app.listen(PORT, ()=>console.log(`Server started at port ${PORT}`));
