5570c391e1d4c7e31f1d5634a5b633bba0dc6ae9
Krishna Gupta <krishna05564@gmail.com>
2024-01-30 16:27:25 +0530

    🗃️ Add password hash to repo

--- src/models/User/index.ts
+++ src/models/User/index.ts
@@ -1,9 +1,16 @@
 import prisma from "../prisma";
 import { User, UserDTO } from "../../services/userService/types";
-
+import bcrypt from "bcrypt";
 export class UserRepository {
+  private SALT_ROUNDS!: number; 
+  UserRepository() {
+    this.SALT_ROUNDS = process.env.SALT_ROUNDS ? parseInt(process.env.SALT_ROUNDS) : 10;
+  }
   async createUser(userRequest: UserDTO): Promise<User> {
     try {
+      // hasing password here
+      // under the assumption that password has been validated to be there
+      userRequest.password = await this.hashPassword(userRequest.password);
       const user = await prisma.user.create({
         data: userRequest,
       });
@@ -24,6 +31,17 @@ export class UserRepository {
       return user;
     } catch (err) {
       console.error(`[UserRepository.getUserById]`, err);
+      throw err;
+    }
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    try{
+      const salt = await bcrypt.genSalt(this.SALT_ROUNDS);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      return hashedPassword;
+    } catch (err) {
+      console.error(`[UserRepository.hashPassword]`, err);
       throw err;
     }
   }
